75d2e6103bda53b90a26603712d407db2029b166
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2023-11-14 14:31:41 +0500

    improved lvim conf and tmux

--- lvim/config.lua
+++ lvim/config.lua
@@ -8,3 +8,15 @@ lvim.plugins = {
 }
 
 lvim.colorscheme = "gruvbox-material"
+
+lvim.format_on_save.enabled = true
+
+lvim.keys.insert_mode["<A-j>"] = false
+lvim.keys.insert_mode["<A-k>"] = false
+lvim.keys.normal_mode["<A-j>"] = false
+lvim.keys.normal_mode["<A-k>"] = false
+lvim.keys.visual_block_mode["<A-j>"] = false
+lvim.keys.visual_block_mode["<A-k>"] = false
+lvim.keys.visual_block_mode["J"] = false
+lvim.keys.visual_block_mode["K"] = false
+
