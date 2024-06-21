2562570515185f8d6f8b511070d8c7826023efe0
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-06-22 01:18:36 +0500

    automatically updated by nixupdate

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -21,7 +21,7 @@
       alias nixrebuild="git -C $BLAZINGLY_FAST add . && sudo nixos-rebuild switch --flake $BLAZINGLY_FAST#workpc"
       alias nixupdate="git -C $BLAZINGLY_FAST add . && git -C $BLAZINGLY_FAST commit -m 'automatically updated by nixupdate' && git -C $BLAZINGLY_FAST push"
       alias nix-shell="INNIXSHELL=true nix-shell --run zsh"
-      alias nixupgrade="sudo nix-channel --update"
+      alias nixupgrade="sudo nix flake update $BLAZINGLY_FAST && nixrebuild"
       if [ -z $INNIXSHELL ] && [ -f shell.nix ]; then
         INNIXSHELL=true nix-shell --run zsh
       fi
