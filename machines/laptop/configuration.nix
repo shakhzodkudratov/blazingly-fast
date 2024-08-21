5fe47dd680ef9539c3833350b29b205fda626310
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-08-21 21:56:10 +0500

    automatically updated by nixupdate

--- machines/laptop/configuration.nix
+++ machines/laptop/configuration.nix
@@ -24,7 +24,23 @@
   '';
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_xanmod;
+    # kernelPackages = pkgs.linuxPackages_xanmod;
+    kernelPatches = [
+      {
+        name = "fix-1";
+        patch = builtins.fetchurl {
+          url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/patch/sound/soc/soc-topology.c?id=e0e7bc2cbee93778c4ad7d9a792d425ffb5af6f7";
+          sha256 = "sha256:1y5nv1vgk73aa9hkjjd94wyd4akf07jv2znhw8jw29rj25dbab0q";
+        };
+      }
+      {
+        name = "fix-2";
+        patch = builtins.fetchurl {
+          url = "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/patch/sound/soc/soc-topology.c?id=0298f51652be47b79780833e0b63194e1231fa34";
+          sha256 = "sha256:14xb6nmsyxap899mg9ck65zlbkvhyi8xkq7h8bfrv4052vi414yb";
+        };
+      }
+    ];
 
     loader = {
       systemd-boot.enable = true;
@@ -93,7 +109,6 @@
     displayManager.sddm.enable = true;
     displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
-
     # https://github.com/Mic92/envfs
     envfs.enable = true;
   };
@@ -106,6 +121,9 @@
       setSocketVariable = true;
     };
   };
+
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "shakhzod" ];
 
   security.rtkit.enable = true;
   services.pipewire = {
